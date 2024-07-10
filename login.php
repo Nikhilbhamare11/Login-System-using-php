f51cd7986201e43d470c46c3878d22afed9d3998
Nikhil Bhamare <147832171+Nikhilbhamare11@users.noreply.github.com>
2024-07-10 13:48:54 +0530

    Update login.php

--- login.php
+++ login.php
@@ -62,11 +62,11 @@
         <form action="/Php Projects/LoginSystem/login.php" method="post">
             <div class="mb-3">
                 <label for="username" class="form-label">UserName</label>
-                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp">
+                <input type="text" class="form-control" id="username" name="username" aria-describedby="emailHelp" required>
             </div>
             <div class="mb-3">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" name="password">
+                <input type="password" class="form-control" id="password" name="password" required>
             </div>
             <button type="submit" class="btn btn-primary">Login</button>
         </form>
