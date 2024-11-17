0121849162d492a04e7888d5e70970f32702926e
Nikhil Bhamare <147832171+Nikhilbhamare11@users.noreply.github.com>
2024-11-17 20:48:11 +0530

    Update _dbconnect.php

--- comp/_dbconnect.php
+++ comp/_dbconnect.php
@@ -4,11 +4,42 @@ $username = "root";
 $password = "";
 $database = "users";
 
-$conn = mysqli_connect($server,$username,$password,$database);
-if (!$conn) {
-//     echo "Success";
-// }
-// else {
-    die("Error in connection". mysqli_connect_error());
+$conn = mysqli_connect($server,$username,$password);
+if(!$conn){
+    die("Connection to this database failed due to ".mysqli_connect_error());
+}
+else{
+    // echo"Success Connecting to the db";
+}
+
+// SQL query to create the database if it doesn't exist
+$sql = "CREATE DATABASE IF NOT EXISTS `$database`";
+
+// Execute the query to create the database
+if (mysqli_query($conn, $sql)) {
+    // echo "Database '$database' created successfully or already exists.<br>";
+
+    // Select the database
+    if (!mysqli_select_db($conn, $database)) {
+        die("Error selecting database: " . mysqli_error($conn));
+    }
+
+}
+else {
+    echo "Error creating database: " . mysqli_error($conn);
+}
+
+// SQL query to create the 'users' table if it doesn't exist
+$sql = "CREATE TABLE IF NOT EXISTS `users` (
+    `sno` INT(8) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    `username` VARCHAR(85) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `datetime` DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL )";
+
+// Execute the query
+if (mysqli_query($conn, $sql)) {
+    // echo "Table 'threads' created successfully or already exists.<br>";
+} else {
+    echo "Error creating table: " . mysqli_error($conn);
 }
 ?>
